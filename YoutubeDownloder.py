e1cb324deafeab619243b00424cafb411f956a42
Md Nazmul Hasan <nazmul7762@gmail.com>
2025-03-21 01:16:56 +0600

    Downloding with ffmpeg

--- YoutubeDownloder.py
+++ YoutubeDownloder.py
@@ -1,5 +1,18 @@
 import os
 import yt_dlp
+import subprocess
+
+
+def check_ffmpeg():
+    try:
+        subprocess.run(["ffmpeg", "-version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except FileNotFoundError:
+        return False
+
+if not check_ffmpeg():
+    print("❌ FFmpeg is not installed!")
+    exit(1)
 
 # Define the playlist URL and directory
 video_url = input("📥 Type YouTube Video/Playlist URL: ").strip()      
@@ -13,13 +26,19 @@ os.makedirs(download_dir, exist_ok=True)
 # Download options for yt-dlp
 ydl_opts = {
     'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+    'format': 'bestvideo[ext=mp4][height<=1080]+bestaudio/best',  # Corrected format for best video & audio
+    'merge_output_format': 'mp4',  # Make sure to merge video and audio into mp4
 }
 
 try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
+    # Zip the directory containing the downloaded videos
+    #shutil.make_archive(download_dir, 'zip', download_dir)
+
     print(f'✅ Video downloaded successfully!')
 
 except Exception as e:
     print(f'❌ An error occurred: {e}')
+
