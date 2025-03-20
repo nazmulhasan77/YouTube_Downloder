192d0db1f84409c61d0d8f138404b61179e56536
Md Nazmul Hasan <nazmul7762@gmail.com>
2025-03-21 01:16:24 +0600

    Downloading code added

---
+++ YoutubeDownloder.py
@@ -0,0 +1,25 @@
+import os
+import yt_dlp
+
+# Define the playlist URL and directory
+video_url = input("📥 Type YouTube Video/Playlist URL: ").strip()      
+
+# Define the download directory
+download_dir = 'YouTube_Download'
+
+# Create the directory if it doesn't exist
+os.makedirs(download_dir, exist_ok=True)
+
+# Download options for yt-dlp
+ydl_opts = {
+    'outtmpl': os.path.join(download_dir, '%(title)s.%(ext)s'),
+}
+
+try:
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([video_url])
+
+    print(f'✅ Video downloaded successfully!')
+
+except Exception as e:
+    print(f'❌ An error occurred: {e}')
